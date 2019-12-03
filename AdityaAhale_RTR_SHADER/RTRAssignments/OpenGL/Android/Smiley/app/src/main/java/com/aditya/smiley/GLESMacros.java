51e51df1e019d1f415241a90ec47b82633c23f99
aditya ahale <adityaahale@gmail.com>
2019-12-03 14:25:57 +0000

    Android Upload

--- AdityaAhale_RTR_SHADER/RTRAssignments/OpenGL/Android/Smiley/app/src/main/java/com/aditya/smiley/GLESMacros.java
+++ AdityaAhale_RTR_SHADER/RTRAssignments/OpenGL/Android/Smiley/app/src/main/java/com/aditya/smiley/GLESMacros.java
@@ -2,6 +2,7 @@ package com.aditya.smiley;
 
 public class GLESMacros
 {
+
 	public static final int AMC_ATTRIBUTE_POSITION = 0;
 	public static final int AMC_ATTRIBUTE_COLOR = 1;
 	public static final int AMC_ATTRIBUTE_NORMAL = 2;
