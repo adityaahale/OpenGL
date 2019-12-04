9f140c3105f80b1c9bc42bf38b53ed08a339a1cc
adityaahale <adityaahale@gmail.com>
2019-12-04 16:16:34 +0530

    Android Upload

--- AdityaAhale_RTR_SHADER/RTRAssignments/OpenGL/Android/Checkerboard/app/src/main/java/com/aditya/checkerboard/GLESMacros.java
+++
@@ -1,9 +0,0 @@
-package com.aditya.checkerboard;
-
-public class GLESMacros
-{
-	public static final int AMC_ATTRIBUTE_POSITION = 0;
-	public static final int AMC_ATTRIBUTE_COLOR = 1;
-	public static final int AMC_ATTRIBUTE_NORMAL = 2;
-	public static final int AMC_ATTRIBUTE_TEXTURE0 = 3;
-}
